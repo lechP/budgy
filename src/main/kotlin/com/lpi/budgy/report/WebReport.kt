b5bfa9ca72dc7082f1c94075e7c3bd21c231664b
Lech Piechota <lech.piechota@ocado.com>
2022-07-15 18:03:59 +0200

    add FreeMarker templates

--- src/main/kotlin/com/lpi/budgy/report/WebReport.kt
+++ src/main/kotlin/com/lpi/budgy/report/WebReport.kt
@@ -2,8 +2,11 @@ package com.lpi.budgy.report
 
 import com.lpi.budgy.domain.Book
 import com.lpi.budgy.domain.Snapshot
+import freemarker.cache.ClassTemplateLoader
+import freemarker.core.HTMLOutputFormat
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
+import io.ktor.server.freemarker.*
 import io.ktor.server.netty.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
@@ -15,9 +18,18 @@ class WebReport(
 
     fun display() {
         embeddedServer(Netty, host = "localhost", port = 2207) {
+            install(FreeMarker) {
+                templateLoader = ClassTemplateLoader(
+                    this::class.java.classLoader, "templates"
+                )
+                outputFormat = HTMLOutputFormat.INSTANCE
+            }
+
             routing {
                 get("/") {
-                    call.respondText("Hello, Budgy!")
+                    call.respond(FreeMarkerContent(
+                        "report.ftl", mapOf("title" to "Budgy")
+                    ))
                 }
             }
         }.start(wait = true)
---
+++ src/main/resources/templates/report.ftl
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8"/>
+    <title>${title}</title>
+</head>
+
+<h1>${title}</h1>
+</html>
